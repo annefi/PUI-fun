634a06f2715f7a99fac2d57db57c4373796478c9
fischer <afischer@physik.uni-kiel.de>
2019-09-25 16:47:36 +0200

    Dies Das

--- Ulysses/filter_He.py
+++ Ulysses/filter_He.py
@@ -8,7 +8,7 @@ I recommend to use a) for H+ and b) for He1+
 
 import matplotlib.pyplot as plt
 import pylab
-from numpy import array, arange, around, argwhere
+from numpy import array, arange, around, argwhere, exp, where, unique
 from DataLoader.uswipha import uswipha
 from pylib import dbData
 import os
@@ -64,9 +64,37 @@ class Filter_He(uswipha):
 
         #pmod = self.hist2d('tch', 'epq', binx=arange(265, 355), biny=arange(21, 55), norm='ymax', weights='brw') #He2+
         #pmod = self.hist2d('tch', 'epq', binx=arange(120,220), biny=arange(0, 22), norm='ymax', weights = 'brw') #H+
-        pmod = self.hist2d('tch', 'epq', binx=arange(250, 420), biny = arange(0, 25), norm = 'ymax') # He1+
+        pmod = self.hist2d('tch', 'epq', binx=arange(340, 500), biny = arange(8, 38), norm = 'ymax') # He1+
         fig = pmod.fig
         cid = fig.canvas.mpl_connect('button_press_event', mouseclick)
+
+
+    def filter_via_mask(self,esa,tch):
+        '''
+        Epq and tch are the resp. data of a mask2D that is drawn manually around He+ in the ESA vs Tch matrix
+        Pass the arguments e.g. as "esa = d.get_data('maskname','epq')"
+        Saving is already included here
+        Attention: Be careful that ESA step 0 is included by drawing the mask below 0 or include it manually
+        '''
+        self.esa_fin = []
+        self.tch_fin = []
+        self.tch_fin_right = []
+        fout = open('data_misc/clicks_epq_ch_he_mask2Ds.txt', 'w')
+        fout.write('Result of the manual clicking for He+ in EpQ vs. Tch'
+                   '\nESA Tch_left Tch_right\n')
+        for esa_i in unique(esa):
+            ind = where(esa == esa_i)[0]
+            t1 = min(tch[ind])
+            t2 = max(tch[ind])
+            fout.write('%i   %f   %f3 \n'%(esa_i, t1, t2))
+            self.esa_fin.append(esa_i)
+            self.tch_fin.append(t1)
+            self.tch_fin_right.append(t2)
+        fout.close()
+
+
+
+
 
     def save_clicks(self, data_type = 'epq', species = 'He1+'):
         '''
@@ -123,7 +151,7 @@ class Filter_He(uswipha):
             self.tch_fin = []
             self.tch_fin_right = []
             if species == 'He1+':
-                fin = open('data_misc/clicks_epq_ch_he.txt', 'r')
+                fin = open('data_misc/clicks_epq_ch_he_mask2D.txt', 'r')
             elif species == 'H+':
                 fin = open('data_misc/clicks_epq_ch_h.txt', 'r')
             elif species == 'He2+':
@@ -157,7 +185,7 @@ class Filter_He(uswipha):
 
     def filter_pha(self, year, dt = 'epq', species = 'He1+'):
         try:
-            self.tch_fin.shape()
+            len(self.tch_fin)
         except:
             self.read_clicks(data_type = dt, species = species)
         for doy in range(1,366):
@@ -212,43 +240,47 @@ class Filter_He(uswipha):
         '''
         test function to check if there is a click difference between years
         '''
-        fid96 = open('data_misc/clicks_epq_ch_he_96.txt', 'r')
-        fid95 = open('data_misc/clicks_epq_ch_he_95.txt', 'r')
-        fid97_99 = open('data_misc/clicks_epq_ch_he_97_99.txt', 'r')
+        fid = open('data_misc/clicks_epq_ch_he_mask2D.txt', 'r')
         for i in range(2):
-            fid96.readline()
-            fid95.readline()
-            fid97_99.readline()
+
+            fid.readline()
         esa = []
-        tch_1_95 = []
-        tch_2_95 = []
-        tch_1_96 = []
-        tch_2_96 = []
-        tch_1_97_99 = []
-        tch_2_97_99 = []
-        for line in fid95:
-            l = line.split()
-            esa.append(float(l[0]))
-            tch_1_95.append(float(l[1]))
-            tch_2_95.append(float(l[2]))
-        # for line in fid96:
+        tch_1 = []
+        tch_2 = []
+
+        # tch_1_95 = []
+        # tch_2_95 = []
+        # tch_1_96 = []
+        # tch_2_96 = []
+        # tch_1_97_99 = []
+        # tch_2_97_99 = []
+        # for line in fid95:
         #     l = line.split()
-        #     tch_1_96.append(float(l[1]))
-        #     tch_2_96.append(float(l[2]))
-        for line in fid97_99:
+        #     esa.append(float(l[0]))
+        #     tch_1_95.append(float(l[1]))
+        #     tch_2_95.append(float(l[2]))
+        # # for line in fid96:
+        # #     l = line.split()
+        # #     tch_1_96.append(float(l[1]))
+        # #     tch_2_96.append(float(l[2]))
+        # for line in fid97_99:
+        #     l = line.split()
+        #     esa.append(float(l[0]))
+        #     tch_1_97_99.append(float(l[1]))
+        #     tch_2_97_99.append(float(l[2]))
+        for line in fid:
             l = line.split()
             esa.append(float(l[0]))
-            tch_1_97_99.append(float(l[1]))
-            tch_2_97_99.append(float(l[2]))
+            tch_1.append(float(l[1]))
+            tch_2.append(float(l[2]))
         self.eesa = esa
-        self.tch_1_95 = tch_1_95
+        self.tch_1 = tch_1
 
         fig, ax = plt.subplots()
 
-        ax.plot(range(len(tch_1_95)), tch_1_95, 'o', label = "95_1")
-        ax.plot(range(len(tch_2_95)), tch_2_95, 'o', label = "95_2")
-        ax.plot(range(len(tch_1_97_99)), tch_1_97_99, 'o', label = "97_99_1")
-        ax.plot(range(len(tch_2_97_99)), tch_2_97_99, 'o', label = "97_99_2")
+        ax.plot(tch_1, esa, 'o', label="")
+        ax.plot(tch_2, esa, 'o', label="")
+
         #plt.yticks(arange(200,400,50))
         ax.legend()
         plt.show()
--- Ulysses/load_pui_ulysses.py
+++ Ulysses/load_pui_ulysses.py
@@ -12,24 +12,24 @@ from matplotlib import pylab
 ###
 
 # load Ulysses data:
-years = [1995]
-#d = uswipha(year=years,tf='all', path = '/home/asterix/fischer/PUI/Ulysses/data_misc/pha_he/epq/')
-d = uswipha(year=years,tf=[[1,20]], path = '/home/asterix/fischer/PUI/Ulysses/data_misc/pha_he2/epq/')
+years = [1994]
+d = uswipha(year=years,tf='all', path = '/home/asterix/fischer/PUI/Ulysses/data_misc/pha_he/epq/')
+#d = uswipha(year=years,tf=[[1,20]], path = '/home/asterix/fischer/PUI/Ulysses/data_misc/pha_he2/epq/')
 d.sync_swoops()
 d.sync_traj()
 
-d.set_mask('Master','rng',0,0,reset=True)
-d.set_mask('Master','det',0,2,reset=True) # cut out det = 3 (=rubbish?)
-d.set_mask('Master','ech',12,250,reset=True) # exclude doubles
-d.set_mask('Master','brw',1,np.inf,reset=True)
-d.set_mask('Master','epq',1,58,reset=True) # transitional mask to match with the ACE velocity steps
-
-# get a real subset with masks applied:
-print('*** Save Subset ***')
-d.save_subset('Master')
-print('*** Load Subset ***')
-d.load_subset(force=True)
-
-D = Dist3D(d)
-from WSpec import WSpec
-ws = WSpec(D,color_norm = 'sg')
+# d.set_mask('Master','rng',0,0,reset=True)
+# d.set_mask('Master','det',0,2,reset=True) # cut out det = 3 (=rubbish?)
+# d.set_mask('Master','ech',12,250,reset=True) # exclude doubles
+# d.set_mask('Master','brw',1,np.inf,reset=True)
+# d.set_mask('Master','epq',1,58,reset=True) # transitional mask to match with the ACE velocity steps
+#
+# # get a real subset with masks applied:
+# print('*** Save Subset ***')
+# d.save_subset('Master')
+# print('*** Load Subset ***')
+# d.load_subset(force=True)
+#
+# D = Dist3D(d)
+# from WSpec import WSpec
+# ws = WSpec(D,color_norm = 'sg')
