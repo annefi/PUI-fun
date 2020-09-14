7b2acb0b857471cfb82312532d5b033ed72a608a
fischer <afischer@physik.uni-kiel.de>
2020-09-14 17:46:03 +0200

    bla

--- Ulysses/Trajectory/SPICE/spice_ulysses.py
+++ Ulysses/Trajectory/SPICE/spice_ulysses.py
@@ -4,15 +4,16 @@ from etspice import kernels
 import datetime
 import spiceypy as spice
 import os
+import numpy as np
 
-os.environ['SPICE_DATA_DIR'] = "../fusessh/data/projects/spice"
+# os.environ['SPICE_DATA_DIR'] = "../fusessh/data/projects/spice"
+os.environ['SPICE_DATA_DIR'] = "/data/projects/spice"
 
 #my_kernel = kernels.LocalKernel('Ulysses/Trajectory/SPICE/metakernel.tm') # load additional kernels via meta kernel
 #my_kernel.load()
 
 HCI = ReferenceFrame([kernels.heliospheric_frames],'HCI')
-
-
+HEEQ = ReferenceFrame([kernels.heliospheric_frames],'HEEQ')
 
 def cart2sph(t, deg=False):
     """
@@ -69,9 +70,7 @@ def locateUlysses(date, RF, spher = True):
     xyz = ULYSSES.position(time = date, relative_to = SUN, reference_frame = RF)
     if spher == True:
         r, theta, phi = utils.cart2spherical(xyz, degrees = True)
-    print(r/1.496e8,phi,theta)
-
-
+    print(r/1.496e8,theta,phi)
 
 def read_pool():
     path_pool = "Ulysses/Trajectory/trajectory_data/traj_data_ulysses_pool.dat"
@@ -84,4 +83,27 @@ def read_pool():
         data = line.split()
         for i,p in enumerate(p_dict.keys()):
             p_dict[p].append(float(data[i]))
+    p_dict["datestring"] = []
+    for i,year in enumerate(p_dict['Year']):
+        p_dict['datestring'].append("%i-%i-%i" % (year,p_dict['MM'][i],p_dict['DD'][i]))
+    for key in p_dict:
+        p_dict[key] = np.array(p_dict[key])
     return p_dict
+
+def get_pool_data(date):
+    '''
+
+    :param date: datetime.datetime object
+    :return:
+    '''
+    data = read_pool()
+    datestring = "%i-%i-%i" % (date.year, date.month, date.day)
+    index = np.where(data['datestring'] == datestring)[0][0]
+    print("HC       R_AU: %s Lat: %s, Long: %s \nHG     R_AU: %s  Lat: %s,  Long: %s \n \n" %(data['R'][index],
+                                                                                         data['HC_Lat'][index],
+                                                                    data['HC_Long'][index],data['R'][index],
+                                                                    data['HG_Lat'][index],data['HG_Long'][index]))
+
+def comp_rf(date):
+    get_pool_data(date)
+    locateUlysses(date, HCI)
